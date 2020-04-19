c7af2483002b879d9b5749fcd0ea487b7a1ec97c
= <=>
2020-04-19 22:51:11 +0300

    add clouds; change sky image

--- js/parallax_init.js
+++ js/parallax_init.js
@@ -1,14 +1,18 @@
 import Parallax from "./main.js";
 
 const firstParallax = new Parallax('.first-bg', {
-    step: 30,
-    zIndex: 1
-}),
- secondParallax = new Parallax('.second-bg', {
-    step: 50,
-    zIndex: 2
-}),
- thirdParallax = new Parallax('.third-bg', {
-    step: 70,
-    zIndex: 3
-});
+        step: 15,
+        zIndex: 1
+    }),
+    secondParallax = new Parallax('.second-bg', {
+        step: 30,
+        zIndex: 2
+    }),
+    thirdParallax = new Parallax('.third-bg', {
+        step: 75,
+        zIndex: 3
+    }),
+    fourthParallax = new Parallax('.fourth-bg', {
+        step: 120,
+        zIndex: 4
+    });
